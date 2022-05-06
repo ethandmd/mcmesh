2062334c87c3b98171abf4b855707a2254f4a6ab
ethandmd <ethandmd@protonmail.com>
2022-05-06 17:19:04 +0100

    Use new create/delete if functions

--- sample.c
+++ sample.c
@@ -11,23 +11,39 @@
 */
 
 int main(int argc, char** argv) {
-    char *if_name = argv[1];
+    const char *if_name = argv[1];
     int ITER = strtol(argv[2], NULL , 10);
-    int if_index = get_ifindex(if_name);
+    int if_index = get_if_index(if_name);
     nl_handle nl;
     sk_handle skh;
     packet_buffer pb;
+    struct if_info original_info;
 
     nl_init(&nl);
     if (!nl.sk) {
         return -1;
     }
 
-    if (check_if_monitor(&nl, if_index) != 1) {
-        printf("Setting device to monitor mode.\n");
-        set_iftype_monitor(&nl, if_index);
+    if (get_if_info(&nl, &original_info, if_index) < 0) {
+        fprintf(stderr, "Could not retriece if information.\n");
+        return -1;
+    }
+    printf("Wiphy: %d\n", original_info.wiphy);
+
+    const char *new_iftype = "monitor";
+    const char *new_ifname = "mcmesh0";
+    int new_if_index;
+    if (compare_if_type(original_info.iftype, new_iftype) == 0) {
+        printf("Device not currently in monitor mode.\n");
+        //set_if_type(&nl, new_iftype, if_index);
+        delete_if(&nl, if_index);
+        printf("Line 40\n");
+        create_new_if(&nl, new_iftype, original_info.wiphy, new_ifname);
+        printf("Line 42\n");
+        new_if_index = get_if_index(new_ifname);
+        printf("Created new monitor mode interface.\n");
     } else {
-        printf("Device already in monitor mode.\n");
+        printf("Device is already in monitor mode.\n");
     }
 
     create_pack_socket(&skh);
@@ -35,7 +51,7 @@ int main(int argc, char** argv) {
         return -1;
     }
     
-    if (bind_pack_socket(&skh, if_index) < 0) {
+    if (bind_pack_socket(&skh, new_if_index) < 0) {
         return -1;
     }
 
@@ -49,7 +65,9 @@ int main(int argc, char** argv) {
     }
     
     printf("Restoring device to managed mode.\n");
-    set_iftype_managed(&nl, if_index);
+    const char *ret_iftype = "managed";
+    //set_if_type(&nl, ret_iftype, if_index);
+    create_new_if(&nl, ret_iftype, original_info.wiphy, if_name);
     nl_cleanup(&nl);
 
     return 0;
