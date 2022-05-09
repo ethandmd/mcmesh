be30f10cf5f65757e0bf6c35b00469dc345f6e8d
ethandmd <ethandmd@protonmail.com>
2022-05-09 12:35:11 -0700

    Remove bpf program from wpt handle

--- wpcap.c
+++ wpcap.c
@@ -9,48 +9,58 @@ void packet_callback(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char
     // char timestr[16];
     // time_t secs;
 
+    printf("Packet callback got called!\n");
     int len = 0;
     while (len < pkthdr->len) {
         printf("%.2X:", *pkt++);
         //Find new lines for 16-byte width packets.
         if (!(++len % 16)) printf("\n");
+        len++;
     }
     printf("\n");
 }
 
-int init_pcap(wifi_pcap_t *wpt, const char *dev_name, const char *filter) {
-    char *dev = pcap_lookupdev(dev_name);
+int init_wpcap(wifi_pcap_t *wpt, const char *dev_name, struct bpf_program *fp, const char *filter) {
 
     //See: https://www.tcpdump.org/linktypes.html for libpcap Link Layer Types.
-    if (!dev) {fprintf(stderr, "Couldn't find %s", dev_name, wpt->errbuf);} //Quick additional check.
+    //if (!dev) {fprintf(stderr, "Couldn't find %s", dev_name, wpt->errbuf);} //Redundant device check when called explicitly with dev.
 
     //For flag (3rd arg), nonzero means promiscuous.
-    wpt->handle = pcap_open_live(dev, BUFSIZ, 1, 1000, wpt->errbuf);
+    printf("Opening pcap session on %s...\n", dev_name);
+    wpt->handle = pcap_open_live(dev_name, BUFSIZ, 1, 1000, wpt->errbuf);
     if (!wpt->handle) {
-        fprintf(stderr, "Could not init pcap handle.\n", wpt->errbuf);
+        fprintf(stderr, "Could not init pcap handle.\n");//, wpt->errbuf);
         return -1;
     }
 
+    printf("Verifying data link type...\n");
     if (pcap_datalink(wpt->handle) != DLT_IEEE802_11_RADIO) {   //versus just DLT_IEEE802_11
-        fprintf(stderr, "%s is not a compatible with radiotap link layer info followed by 80211 header.\n", dev_name, wpt->errbuf);
+        fprintf(stderr, "%s is not a compatible with radiotap link layer info followed by 80211 header.\n", dev_name);//, wpt->errbuf);
         return -1;
     }
 
-    struct bpf_program fp;
-    bpf_int32 netmask; //=0xffffff // ==> C class network.
-    if (pcap_compile(wpt->handle, &fp, filter, 0, netmask) < 0) {
-        fprintf(stderr, "Could not compile BPF expression:\n\t%s.\n", filter, wpt->errbuf);
+    printf("Compiling bpf filter: '%s'...\n", filter);
+    bpf_int32 netmask = 0; //0xffffff; // ==> C class network.
+    if (pcap_compile(wpt->handle, fp, filter, 0, netmask) < 0) {
+        fprintf(stderr, "Could not compile BPF expression:\n\t%s.\n", filter);//, wpt->errbuf);
         return -1;
     }
-    if (pcap_setfilter(wpt->handle, &fp) < 0) {
-        fprintf(stderr, "Could not apply BPF fitler.\n", wpt->errbuf);
+    printf("Setting filter...\n");
+    if (pcap_setfilter(wpt->handle, fp) < 0) {
+        fprintf(stderr, "Could not apply BPF fitler.\n");//, wpt->errbuf);
         //return -1;
     }
 }
 
+void cleanup_wpcap(wifi_pcap_t *wpt, struct bpf_program *fp) {
+    pcap_freecode(fp);
+    pcap_close(wpt->handle);
+}
+
 void view_packets(wifi_pcap_t  *wpt, int ITER) {
+    printf("Waiting for packets...\n");
     int ret = pcap_loop(wpt->handle, ITER, packet_callback, NULL);
     if (ret < 0) {
-        fprintf(stderr, "Unable to capture %d packets.\n", ITER, wpt->errbuf);
+        fprintf(stderr, "Unable to capture %d packets.\n", ITER);//, wpt->errbuf);
     }
 }
--- wpcap.h
+++ wpcap.h
@@ -5,6 +5,8 @@ typedef struct {
     char errbuf[PCAP_ERRBUF_SIZE];
 } wifi_pcap_t;
 
-void init_pcap(wifi_pcap_t *wpt, const char *dev_name, const char *filter);
+int init_wpcap(wifi_pcap_t *wpt, const char *dev_name, struct bpf_program *fp, const char *filter);
+
+void cleanup_wpcap(wifi_pcap_t *wpt, struct bpf_program *fp);
 
 void view_packets(wifi_pcap_t *wpt, int ITER);
